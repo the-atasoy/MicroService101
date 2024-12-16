5fcee20394ba6f2f40f35428fb3dd61061a1864c
the-atasoy <barisatasoy2917@gmail.com>
2024-12-17 00:11:25 +0300

    controller is done

--- PlatformService/Controllers/PlatformController.cs
+++ PlatformService/Controllers/PlatformController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data.Repository.Platform;
 using PlatformService.Dtos.Platform;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers;
 
@@ -14,5 +15,20 @@ public class PlatformController(IPlatformRepository repository, IMapper mapper) 
     {
         var platforms = await repository.GetAllAsync();
         return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<PlatformReadDto>> GetById(Guid id)
+    {
+        var platform = await repository.GetByIdAsync(id);
+        return platform == null ? NotFound() : Ok(mapper.Map<PlatformReadDto>(platform));
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> Create(PlatformCreateDto platformCreateDto)
+    {
+        await repository.CreateAsync(mapper.Map<Platform>(platformCreateDto));
+        await repository.SaveChangesAsync();
+        return StatusCode(StatusCodes.Status201Created);
     }
 }
